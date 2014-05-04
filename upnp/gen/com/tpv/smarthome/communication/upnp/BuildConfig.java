9347b8982eb6d5ac6bc51c8c39115666dcb330e3
europelee <europe.lee@gmail.com>
2014-05-04 09:28:43 +0800

    ignore gen and rm

--- upnp/gen/com/tpv/smarthome/communication/upnp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.tpv.smarthome.communication.upnp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
